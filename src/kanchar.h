37a1114680fa536cd3b09229f81c07381321097e
KIHARA Hideto <deton@m1.interq.or.jp>
2016-06-18 09:59:42 +0900

    gh-pages用にindex.html追加とhtml関係ファイルのみに

--- src/kanchar.h
+++
@@ -1,8 +0,0 @@
-#define EXPORT extern "C" __declspec(dllexport)
-
-EXPORT BOOL IsHooking(void);
-EXPORT void MySetHook(HHOOK *, HHOOK *);
-EXPORT int MyEndHook(void);
-EXPORT LRESULT CALLBACK cwpHookProc(int, WPARAM, LPARAM);
-EXPORT LRESULT CALLBACK msgHookProc(int, WPARAM, LPARAM);
-
